2dc124a2ce4902835dcf5b373627dcec57a2582c
Tijmen Smit <tijmen@wpstorelocator.co>
2026-09-26 15:55:40 +0300

    Fix comment spelling in my_mapbox_cluster_colors function

--- wpsl_cluster_marker_props.php
+++ wpsl_cluster_marker_props.php
@@ -2,7 +2,7 @@ add_filter( 'wpsl_cluster_marker_props', 'my_mapbox_cluster_colors' );
 
 function my_mapbox_cluster_colors( $props ) {
     if ( isset( $props['circle'] ) ) {
-        // Colour below 100, threshold, colour 100 - 750, threshold, colour above 750.
+        // Color below 100, threshold, colour 100 - 750, threshold, colour above 750.
         $props['circle']['color'] = [ '#2b83ba', 100, '#fdae61', 750, '#d7191c' ];
     }
 
